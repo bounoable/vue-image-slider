1aec7270ad890437d98fdb8139b13e0f65bc5a44
Saman Hosseini <bounoable@gmail.com>
2019-10-24 15:27:20 +0200

    setup

---
+++ dev/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+
+export default createComponent({
+  
+})
+</script>
+
+<template>
+<div>
+  
+</div>
+</template>
---
+++ dev/main.ts
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import CompositionApi from '@vue/composition-api'
+import App from './App.vue'
+
+Vue.use(CompositionApi)
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
